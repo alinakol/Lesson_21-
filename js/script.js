443988b5dd41d90521a94ca0a704105107cd0415
alinakol <65068381+alinakol@users.noreply.github.com>
2022-11-30 23:59:30 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,57 @@
+"use strict"
+
+//console.log('HELLO');
+//console.log('Age');
+
+let myName;
+console.log(typeof myName);
+
+let age = 27;
+
+age = String(age)
+console.log(age);
+console.log(typeof age);
+
+
+
+/* age = myName;
+console.log(age); */
+
+testBlock()
+function testBlock() {
+	let mySuname = "Kolesnikova";
+	console.log(mySuname);
+	console.log(typeof mySuname);
+}
+
+let trueOfFalse = 59 < 18;
+console.log(trueOfFalse);
+
+let getInfinitiNumber = "45" / "6";
+console.log(getInfinitiNumber);
+console.log(typeof getInfinitiNumber);
+
+let getInfiniti = 45 / 0;
+console.log(getInfiniti);
+console.log(typeof getInfiniti);
+
+let userAge = 27;
+let userAgeInfo = `Колесникова: ${userAge}`;
+console.log(userAgeInfo);
+console.log(typeof userAgeInfo);
+
+
+//Д/З Основы синтаксиса JAVASCRIPT. Структура кода. Инструкции. Режим use strict.
+
+const USER_EYE_COLOR = "#281a1a"
+console.log(USER_EYE_COLOR);
+
+let user;
+let userName = "Вася"
+user = userName;
+console.log(user)
+
+
+
+
+
